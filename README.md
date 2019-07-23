2bc188c28ab810b2c84f4851730c0edfdbfb4669
toooooodo <1042178105@qq.com>
2019-07-23 19:21:46 +0800

    完善Dataset

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 论文传送门：[Distributed Representations of Words and Phrases and their Compositionality](https://papers.nips.cc/paper/5021-distributed-representations-of-words-and-phrases-and-their-compositionality)
 
 参考资料：[《动手学深度学习》10.3节](http://zh.d2l.ai/chapter_natural-language-processing/word2vec-gluon.html)
+
+数据集：[Penn Tree Bank](https://catalog.ldc.upenn.edu/LDC99T42)
--- dataset.py
+++ dataset.py
@@ -13,17 +13,14 @@ class PTBDataSet(Dataset):
         self.t = 1e-4
         self.max_window_size = 5
         self.negative_sampling_num = 5
-
-        self.all_centers, self.all_contexts, self.all_negatives = self.load_text()
-        print('center',self.all_centers.shape)
-        print('contexts', self.all_contexts.shape)
-        print('negatives', self.all_negatives.shape)
+        self.centers, self.contexts_negatives, self.masks, self.labels = self.load_text()
+        print(self.centers.shape, self.contexts_negatives.shape, self.masks.shape, self.labels.shape)
 
     def __len__(self):
-        pass
+        return self.centers.shape[0]
 
-    def __getitem__(self, item):
-        pass
+    def __getitem__(self, index):
+        return [self.centers[index], self.contexts_negatives[index], self.masks[index], self.labels[index]]
 
     def load_text(self):
         with open('./data/ptb.train.txt', 'r') as f:
@@ -65,8 +62,9 @@ class PTBDataSet(Dataset):
                         [negativeN]]
         """
         all_negatives = self.get_negatives(all_contexts, sampling_weights, self.negative_sampling_num)
+        return self.padding(all_centers=all_centers, all_contexts=all_contexts, all_negatives=all_negatives)
         # return all_centers, all_contexts, all_negatives
-        return np.array(all_centers), np.array(all_contexts), np.array(all_negatives)
+        # return np.array(all_centers), np.array(all_contexts), np.array(all_negatives)
 
     def discard(self, token_index, counter, index_to_token, token_num):
         return random.uniform(0, 1) < max(
@@ -81,6 +79,7 @@ class PTBDataSet(Dataset):
             for center_word_index in range(len(sentence)):
                 # [1, max_window_size]
                 window_size = random.randint(1, max_window_size)
+                # [center_word - window_size, center_word + window_size]
                 indices = list(
                     range(max([0, center_word_index - window_size]),
                           min([len(sentence), center_word_index + window_size + 1])))
@@ -89,6 +88,14 @@ class PTBDataSet(Dataset):
         return centers, contexts
 
     def get_negatives(self, all_contexts, sampling_weights, K):
+        """
+        我们使用负采样来进行近似训练。对于一对中心词和背景词，我们随机采样 K 个噪声词（实验中设 K=5 ）。
+        根据word2vec论文的建议，噪声词采样概率 P(w) 设为 w 词频与总词频之比的0.75次方
+        :param all_contexts: 正样本
+        :param sampling_weights: 采样概率
+        :param K: K个噪声词
+        :return:
+        """
         all_negatives, neg_candidates, i = [], [], 0
         population = list(range(len(sampling_weights)))
         for context in all_contexts:
@@ -103,6 +110,34 @@ class PTBDataSet(Dataset):
             all_negatives.append(negatives)
         return all_negatives
 
+    def padding(self, all_centers, all_contexts, all_negatives):
+        """
+        填充每一个训练样本
+        :param all_centers:
+        :param all_contexts:
+        :param all_negatives:
+        :return: centers: 样本中心词，shape: (centers,)
+                 contexts_negatives：样本背景词与噪声词，shape: (centers, max_len)
+                 masks：1 => 不是填充项；0 => 填充项, shape: (centers, max_len)
+                 labels：1 => 正样本（背景词）；0 => 负样本（噪声词），shape: (centers, max_len)
+        """
+        max_len = self.max_window_size * 2 + self.max_window_size * 2 * self.negative_sampling_num
+        centers, contexts_negatives, masks, labels = [], [], [], []
+        for center, context, negative in zip(all_centers, all_contexts, all_negatives):
+            cur_len = len(context) + len(negative)
+            centers.append(center)
+            contexts_negatives += [context + negative + [0] * (max_len - cur_len)]
+            masks += [cur_len * [1] + (max_len - cur_len) * [0]]
+            labels += [len(context) * [1] + (max_len - len(context)) * [0]]
+        return np.array(centers), np.array(contexts_negatives), np.array(masks), np.array(labels)
+
 
 if __name__ == '__main__':
-    Data = PTBDataSet()
+    train_set = PTBDataSet()
+    train_loader = DataLoader(train_set, batch_size=512, shuffle=True)
+    for batch_idx, (center, context_negative, mask, label) in enumerate(train_loader):
+        print('center', center.shape)
+        print('context_nagative',context_negative.shape)
+        print('mask', mask.shape)
+        print('label', label.shape)
+        break
