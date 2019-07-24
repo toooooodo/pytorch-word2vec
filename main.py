d2ef59a16be81b194a10ffe64f5007e77b7136ea
toooooodo <1042178105@qq.com>
2019-07-24 15:30:10 +0800

    完善训练代码

--- main.py
+++ main.py
@@ -7,21 +7,32 @@ from torch.utils.data import DataLoader
 
 embedding_dim = 100
 lr = 5e-3
+batch_size = 512
+epochs = 5
 
 
 def main():
     train_set = PTBDataSet()
-    train_loader = DataLoader(train_set, batch_size=512, shuffle=True)
-    device = torch.device('cuda')
+    train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
+    device = torch.device('cpu')
     model = SkipGram(train_set.get_token_num(), embedding_dim).to(device)
     optimizer = optim.Adam(model.parameters(), lr=lr)
-    for batch_idx, (center, context_negative, mask, label) in enumerate(train_loader):
-        center, context_negative, mask, label = center.to(device), context_negative.to(device), mask.to(
-            device), label.to(device)
-        # model.train()
-        loss = nn.BCEWithLogitsLoss(weight=mask, reduction='none').to(device)
-        pred = model(center, context_negative)
-        break
+    for epoch in range(epochs):
+        total_loss = 0
+        for batch_idx, (center, context_negative, mask, label) in enumerate(train_loader):
+            center, context_negative, mask, label = center.to(device), context_negative.to(device), mask.to(
+                device), label.to(device)
+            criteon = nn.BCEWithLogitsLoss(weight=mask.float(), reduction='none').to(device)
+            # pred: [batch_size, max_len]
+            pred = model(center, context_negative)
+            loss = torch.sum(torch.sum(criteon(pred.float(), label.float()), dim=1) / torch.sum(mask.float(), dim=1))
+            total_loss += loss.item()
+            if batch_idx % 20 == 0:
+                print(f'epoch {epoch+1} batch {batch_idx} loss {loss.item()/pred.shape[0]}')
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+        print(f'epoch {epoch+1} average loss {total_loss/train_set.__len__()}')
 
 
 if __name__ == '__main__':
--- model.py
+++ model.py
@@ -9,5 +9,15 @@ class SkipGram(nn.Module):
         self.embed_u = nn.Embedding(num_embeddings=num_embeddings, embedding_dim=embedding_dim)
 
     def forward(self, center, context_negative):
-        print(self.embed_v(center).shape)
-        # print(torch.transpose(self.embed_u(context_negative), 1, 2).shape)
+        """
+        在前向计算中，跳字模型的输入包含中心词索引center以及连结的背景词与噪声词索引contexts_and_negatives。
+        其中center变量的形状为(批量大小,)，而contexts_and_negatives变量的形状为(批量大小, max_len)。
+        这两个变量先通过词嵌入层分别由词索引变换为词向量，再通过小批量乘法得到形状为(批量大小, 1, max_len)的输出。
+        输出中的每个元素是中心词向量与背景词向量或噪声词向量的内积。
+        :param center: [batch_size]
+        :param context_negative: [batch_size, max_len]
+        :return: [batch_size, max_len]
+        """
+        out = torch.bmm(torch.unsqueeze(self.embed_v(center), 1), torch.transpose(self.embed_u(context_negative), 1, 2))
+        out = torch.squeeze(out)
+        return out
